16dd24870cd5fd12b5180dae6983553fc6acfd76
Luissam16 <luissam1625@outlook.es>
2024-04-30 23:44:34 -0500

    Feat: Carrusel de imagenes para restaurante

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:food_delievery_restaurants/controller/provider/authProvider/MobileAuthProvider.dart';
+import 'package:food_delievery_restaurants/controller/provider/restaurantRegisterProvider/restaurantRegisterProvider.dart';
 import 'package:food_delievery_restaurants/firebase_options.dart';
 import 'package:food_delievery_restaurants/view/signInLogicScreen/signInLoginScreen.dart';
 
@@ -25,6 +26,9 @@ class FoodDelievery extends StatelessWidget {
         providers: [
           ChangeNotifierProvider<MobileAuthProvider>(
             create: (_) => MobileAuthProvider(),
+          ),
+          ChangeNotifierProvider<RestaurantRegisterProvider>(
+            create: (_) => RestaurantRegisterProvider(),
           ),
         ],
         child: MaterialApp(
--- lib/view/restaurantRegistrationScreen/restaurantRegistrationScreen.dart
+++ lib/view/restaurantRegistrationScreen/restaurantRegistrationScreen.dart
@@ -1,22 +1,29 @@
+import 'dart:io';
+
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:food_delievery_restaurants/controller/services/imageServices/imagesServices.dart';
+import 'package:food_delievery_restaurants/controller/provider/restaurantRegisterProvider/restaurantRegisterProvider.dart';
 import 'package:food_delievery_restaurants/utils/colors.dart';
 import 'package:food_delievery_restaurants/utils/textStyles.dart';
 import 'package:food_delievery_restaurants/widgets/textfieldWidget.dart';
+import 'package:provider/provider.dart';
 import 'package:sizer/sizer.dart';
 
 class RestaurantRegistrationScreen extends StatefulWidget {
   const RestaurantRegistrationScreen({super.key});
 
   @override
-  State<RestaurantRegistrationScreen> createState() => _RestaurantRegistrationScreen();
+  State<RestaurantRegistrationScreen> createState() =>
+      _RestaurantRegistrationScreen();
 }
 
-class _RestaurantRegistrationScreen extends State<RestaurantRegistrationScreen> {
+class _RestaurantRegistrationScreen
+    extends State<RestaurantRegistrationScreen> {
   TextEditingController restaurantNameController = TextEditingController();
-  TextEditingController restaurantLicenceNumberController = TextEditingController();
-
+  TextEditingController restaurantLicenceNumberController =
+      TextEditingController();
+  CarouselController controller = CarouselController();
 
   @override
   Widget build(BuildContext context) {
@@ -27,47 +34,84 @@ class _RestaurantRegistrationScreen extends State<RestaurantRegistrationScreen> 
         SizedBox(
           height: 2.h,
         ),
-        InkWell(
-          onTap: ()async{
-            await ImageServices.getImagesFromGallery(context: context);
-          },
-          child: Container(
-            height: 20.h,
-            width: 94.w,
-            alignment: Alignment.center,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(5.sp),
-              color: greyShade3,
-            ),
-            child: Column(mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Container(
-                  height: 5.h,
-                  width: 5.h,
-                  alignment: Alignment.center,
-                  decoration: BoxDecoration(
-                    shape: BoxShape.circle,
-                    border: Border.all(
-                      color: black,
+        Consumer<RestaurantRegisterProvider>(
+            builder: (context, registrationProvider, child) {
+          if (registrationProvider.restaurantBannerImages.isEmpty) {
+            return InkWell(
+              onTap: () async {
+                await registrationProvider.getRestaurantBannerImages(context);
+              },
+              child: Container(
+                height: 20.h,
+                width: 94.w,
+                alignment: Alignment.center,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(5.sp),
+                  color: greyShade3,
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      height: 5.h,
+                      width: 5.h,
+                      alignment: Alignment.center,
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        border: Border.all(
+                          color: black,
+                        ),
+                      ),
+                      child: FaIcon(
+                        FontAwesomeIcons.plus,
+                        size: 3.h,
+                        color: black,
+                      ),
                     ),
-                  ),
-                  child: FaIcon(
-                    FontAwesomeIcons.plus,
-                    size: 3.h,
-                    color: black,
-                  ),
+                    SizedBox(
+                      height: 1.h,
+                    ),
+                    Text(
+                      'Agregar',
+                      style: AppTextStyles.body14,
+                    )
+                  ],
                 ),
-                SizedBox(
-                  height: 1.h,
+              ),
+            );
+          } else {
+            List<File> bannerImages =
+                registrationProvider.restaurantBannerImages;
+            return Container(
+              height: 23.h,
+              width: 94.w,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(5.sp),
+                  border: Border.all(color: greyShade3)),
+              child: CarouselSlider(
+                carouselController: controller,
+                options: CarouselOptions(
+                  height: 23.h,
+                  autoPlay: true,
+                  viewportFraction: 1,
                 ),
-                Text(
-                  'Agregar',
-                  style: AppTextStyles.body14,
-                )
-              ],
-            ),
-          ),
-        ),
+                items: bannerImages
+                    .map(
+                      (image) => Container(
+                        width: 94.w,
+                        decoration: BoxDecoration(
+                          image: DecorationImage(
+                            image: FileImage(image),
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      ),
+                    )
+                    .toList(),
+              ),
+            );
+          }
+        }),
         SizedBox(
           height: 4.h,
         ),
@@ -89,7 +133,6 @@ class _RestaurantRegistrationScreen extends State<RestaurantRegistrationScreen> 
         SizedBox(
           height: 2.h,
         ),
-
       ],
     ));
   }
